07c7070a1bb3dac236ba6cebf1b452a17a427139
mrios01 <m.angel.rios@gmail.com>
2021-03-28 17:12:04 -0600

    1 file modified

--- src/app/jsnumber/jsnumber.component.ts
+++ src/app/jsnumber/jsnumber.component.ts
@@ -15,11 +15,11 @@ export class JsnumberComponent implements OnInit {
 
   onTransform() {
     // System for American Numbering
-    var th_val = ["", "thousand", "million", "billion", "trillion"];
+    let th_val = ["", "thousand", "million", "billion", "trillion"];
     // System for uncomment this line for Number of English
-    // var th_val = ['','thousand','million', 'milliard','billion'];
+    // let th_val = ['','thousand','million', 'milliard','billion'];
 
-    var dg_val = [
+    let dg_val = [
       "zero",
       "one",
       "two",
@@ -31,7 +31,7 @@ export class JsnumberComponent implements OnInit {
       "eight",
       "nine"
     ];
-    var tn_val = [
+    let tn_val = [
       "ten",
       "eleven",
       "twelve",
@@ -43,7 +43,7 @@ export class JsnumberComponent implements OnInit {
       "eighteen",
       "nineteen"
     ];
-    var tw_val = [
+    let tw_val = [
       "twenty",
       "thirty",
       "forty",
@@ -54,47 +54,49 @@ export class JsnumberComponent implements OnInit {
       "ninety"
     ];
 
-    function toWordsconver(s) {
-      s = s.toString();
-      s = s.replace(/[\, ]/g, "");
-      if (s != parseFloat(s)) return "not a number ";
-      var x_val = s.indexOf(".");
-      if (x_val == -1) x_val = s.length;
-      if (x_val > 15) return "too big";
-      var n_val = s.split("");
-      var str_val = "";
-      var sk_val = 0;
-      for (var i = 0; i < x_val; i++) {
-        if ((x_val - i) % 3 == 2) {
-          if (n_val[i] == "1") {
-            str_val += tn_val[Number(n_val[i + 1])] + " ";
-            i++;
-            sk_val = 1;
-          } else if (n_val[i] != 0) {
-            str_val += tw_val[n_val[i] - 2] + " ";
-            sk_val = 1;
-          }
+    // function toWordsconver(s) {
+    //s = s.toString();
+    let s: any = this.numtoletter;
+    s = s.replace(/[\, ]/g, "");
+    if (s != parseFloat(s)) return "not a number ";
+    let x_val = s.indexOf(".");
+    if (x_val == -1) x_val = s.length;
+    if (x_val > 15) return "too big";
+    let n_val = s.split("");
+    let str_val = "";
+    let sk_val = 0;
+    for (let i = 0; i < x_val; i++) {
+      if ((x_val - i) % 3 == 2) {
+        if (n_val[i] == "1") {
+          str_val += tn_val[Number(n_val[i + 1])] + " ";
+          i++;
+          sk_val = 1;
         } else if (n_val[i] != 0) {
-          str_val += dg_val[n_val[i]] + " ";
-          if ((x_val - i) % 3 == 0) str_val += "hundred ";
+          str_val += tw_val[n_val[i] - 2] + " ";
           sk_val = 1;
         }
-        if ((x_val - i) % 3 == 1) {
-          if (sk_val) str_val += th_val[(x_val - i - 1) / 3] + " ";
-          sk_val = 0;
-        }
+      } else if (n_val[i] != 0) {
+        str_val += dg_val[n_val[i]] + " ";
+        if ((x_val - i) % 3 == 0) str_val += "hundred ";
+        sk_val = 1;
       }
-      if (x_val != s.length) {
-        var y_val = s.length;
-        str_val += "point ";
-        for (var i: number = x_val + 1; i < y_val; i++)
-          str_val += dg_val[n_val[i]] + " ";
+      if ((x_val - i) % 3 == 1) {
+        if (sk_val) str_val += th_val[(x_val - i - 1) / 3] + " ";
+        sk_val = 0;
       }
-      return str_val.replace(/\s+/g, " ");
     }
+    if (x_val != s.length) {
+      let y_val = s.length;
+      str_val += "point ";
+      for (let i: number = x_val + 1; i < y_val; i++)
+        str_val += dg_val[n_val[i]] + " ";
+    }
+    //return str_val.replace(/\s+/g, " ");
+    this.phrase = this.numtoletter + " == " + str_val.replace(/\s+/g, " ");
+    //}
 
-    console.log(this.numtoletter);
-    console.log(this.phrase);
-    this.phrase = "Hello " + toWordsconver(this.numtoletter);
+    //console.log(this.numtoletter);
+    //console.log(this.phrase);
+    //this.phrase = "Hello " + toWordsconver(this.numtoletter);
   }
 }
